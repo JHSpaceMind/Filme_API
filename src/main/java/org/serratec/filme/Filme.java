dfce531f76cef4fea7613368a8a38d562f8b9bbf
juliahx0 <juliaxavier.134@gmail.com>
2024-10-17 19:18:42 -0300

    Teste

---
+++ src/main/java/org/serratec/filme/Filme.java
@@ -0,0 +1,79 @@
+package org.serratec.filme;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Filme {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+ @NotBlank
+ @Size(min=2 ,message = "O nome do filme deve ter pelo menos 2 caracteres")
+ private String nome;
+ @Enumerated(EnumType.STRING)
+ private Genero genero;
+ @NotNull
+ @Past(message ="A data de lançamento do filme deve ser inferior ao dia de hoje")
+ private LocalDate anoLancamento;
+ 
+ @OneToOne(cascade = CascadeType.ALL)
+ private Diretor diretor;
+ 
+ @OneToMany(mappedBy = "filme", cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+ private List<Ator> atores;
+ 
+ 
+public List<Ator> getAtores() {
+	return atores;
+}
+public void setAtores(List<Ator> atores) {
+	atores.forEach(a -> a.setFilme(this));
+	this.atores = atores;
+}
+public Diretor getDiretor() {
+	return diretor;
+}
+public void setDiretor(Diretor diretor) {
+	this.diretor = diretor;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public LocalDate getAnoLancamento() {
+	return anoLancamento;
+}
+public void setAnoLancamento(LocalDate anoLancamento) {
+	this.anoLancamento = anoLancamento;
+}
+public Genero getGenero() {
+	return genero;
+}
+public void setGenero(Genero genero) {
+	this.genero = genero;
+}
+}
---
+++ src/main/java/org/serratec/filme/Genero.java
@@ -0,0 +1,12 @@
+package org.serratec.filme;
+
+public enum Genero {
+  COMEDIA,
+  TERROR,
+  DRAMA,
+  ACAO,
+  SUSPENSE,
+  FANTASIA,
+  ANIMACAO,
+  ROMANCE
+}
